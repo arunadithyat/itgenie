7d25443de5e17ecd4c19d721eb12d7969fd9d812
Arun <arunadithyat@gmail.com>
2025-04-26 17:16:24 +0530

    Cleared Vers

--- itgenie/hooks.py
+++ itgenie/hooks.py
@@ -56,7 +56,15 @@ app_license = "mit"
 # Home Pages
 # ----------
 
-fixtures = ["Custom Field"]
+fixtures = [
+    {
+        "dt": "Custom Field",
+        "filters": {
+            "module": ["in", ["Itgenie"]]
+        }
+    }
+]
+
 # application home page (will override Website Settings)
 # home_page = "login"
 
